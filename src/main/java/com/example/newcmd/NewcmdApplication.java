1bb91123562a8165fbed2a0bc20c0e24351de573
wupengfeishiro <1930369489@qq.com>
2019-09-29 11:28:47 +0800

    Initial commit

---
+++ src/main/java/com/example/newcmd/NewcmdApplication.java
@@ -0,0 +1,13 @@
+package com.example.newcmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewcmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewcmdApplication.class, args);
+	}
+
+}
